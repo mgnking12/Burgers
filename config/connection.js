1b0ed80e756d8b83183c28158ad31335c8585c9a
Megan King <mgnking12@gmail.com>
2016-08-03 08:59:54 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
 var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-
-var connection;
 
 if (process.env.JAWSDB_URL) {
     // connection = mysql.createConnection(process.env.JAWSDB_URL);
