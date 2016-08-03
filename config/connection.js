9f8b1887040a3a0de4737c254d8e35e00e3c7f3d
Megan King <mgnking12@gmail.com>
2016-08-02 20:45:56 -0500

    heroku fix

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,20 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    // connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection({
+        host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'bvos70b8raxzvzvc',
+        password: 'tgfj6ivlbnv4k172',
+        database: 'ghep383e0msp498s'
+    });
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'burgers_db'
-    })
+    });
 }
 
 connection.connect(function(err) {
