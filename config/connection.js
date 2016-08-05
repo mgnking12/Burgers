2a6dfb11c7af1d33dbe1a3369f0a3d6555f24de2
Megan King <mgnking12@gmail.com>
2016-08-05 15:14:48 -0500

    I think it will deploy now, fingers crossed

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,4 @@
-/*
-Here is where you make the connection to the database and export and used by the O.R.M.
-*/
 var mysql = require('mysql');
-
 
 var connection = mysql.createConnection({
     port: 3306,
